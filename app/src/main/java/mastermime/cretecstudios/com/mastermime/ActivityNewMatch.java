9d73ac542dcce6c468a1538add0ae586a425fd68
vinicios-biluca <vinicios-biluca@outlook.com>
2017-10-17 21:12:57 -0200

    Salvar partida ok.

---
+++ app/src/main/java/mastermime/cretecstudios/com/mastermime/ActivityNewMatch.java
@@ -0,0 +1,154 @@
+package mastermime.cretecstudios.com.mastermime;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class ActivityNewMatch extends AppCompatActivity {
+
+    SeekBar turn, time;
+    TextView turns, times;
+    EditText equipe1, equipe2;
+    Button ok, cancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_match);
+
+        findMyViews();
+
+    }
+
+    public void insertMatch() {
+
+        double tu = 5.0 + (2 * turn.getProgress());
+        double ti = 60.0 + (30 * time.getProgress());
+
+        String team1 = equipe1.getText().toString();
+        String team2 = equipe2.getText().toString();
+
+        ObjectMatch match = new ObjectMatch();
+        match.setTurns(tu);
+        match.setTime(ti);
+        match.setTeam1(team1);
+        match.setTeam2(team2);
+
+        DaoSession daoSession = ((AppORM) getApplication()).getDaoSession();
+
+        daoSession.getObjectMatchDao().insert(match);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Partida Salva!", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public void findMyViews() {
+
+        ok = (Button) findViewById(R.id.bt_ok_match);
+        cancel = (Button) findViewById(R.id.bt_cancel_match);
+
+        equipe1 = (EditText) findViewById(R.id.edt_equipe1);
+        equipe2 = (EditText) findViewById(R.id.edt_equipe2);
+
+        turns = (TextView) findViewById(R.id.txt_turnos);
+        times = (TextView) findViewById(R.id.txt_time);
+
+        turn = (SeekBar) findViewById(R.id.sb_turns);
+        time = (SeekBar) findViewById(R.id.sb_time);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (testaValores()) {
+
+                    insertMatch();
+
+                }
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        turn.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int turn_count = 5 + (2 * progress);
+
+                turns.setText("Turnos: " + turn_count);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int time_count = 60 + (30 * progress);
+
+                times.setText("Tempo da jogada: " + time_count + "s");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+    public boolean testaValores() {
+        boolean test = true;
+
+        if (equipe1.getText().toString().equals("")) {
+            equipe1.setError("Digite o nome da Equipe 1!");
+            test = false;
+        }
+
+        if (equipe2.getText().toString().equals("")) {
+            equipe2.setError("Digite o nome da Equipe 2!");
+            test = false;
+        }
+
+        return test;
+    }
+
+}
+
+
+
---
+++ app/src/main/java/mastermime/cretecstudios/com/mastermime/ActivityPlayRoom.java
@@ -0,0 +1,19 @@
+package mastermime.cretecstudios.com.mastermime;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ActivityPlayRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playroom);
+
+    }
+
+}
